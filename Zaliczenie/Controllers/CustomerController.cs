1a936e3ec45f9ad4ab047e9475b18038e9823696
Dawid Slapek <dawid.slapek@gmail.com>
2018-09-24 12:35:24 +0200

    First comit

---
+++ Zaliczenie/Controllers/CustomerController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Zaliczenie.Models;
+
+namespace Zaliczenie.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult XmlDisplay()
+        {
+            var data = new List<PrzykladowyModel>();
+            data = ZwrocDane();
+            return View(data);
+
+
+        }
+
+        private List<PrzykladowyModel> ZwrocDane()
+        {
+            string xmldane = Server.MapPath("~/XMLFile/Data.xml");
+            DataSet ds = new DataSet();
+            ds.ReadXml(xmldane);
+
+            var customerlist = new List<PrzykladowyModel>();
+            customerlist =
+            (
+              from rows in ds.Tables[0].AsEnumerable()
+              select new PrzykladowyModel
+              {
+                 Id = Convert.ToInt32(rows[0].ToString()),
+                 Imie = rows[1].ToString(),
+                 Nazwisko = rows[2].ToString(),
+                 Telefon = Convert.ToInt32(rows[3].ToString()),
+              }
+            ).ToList();
+            return customerlist;
+
+        }
+    }
+}
---
+++ Zaliczenie/Models/PrzykladowyModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Zaliczenie.Models
+{
+    public class PrzykladowyModel
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string Imie
+        {
+            get;
+            set;
+        }
+        public string Nazwisko
+        {
+            get;
+            set;
+            
+        }
+        public int Telefon
+        {
+            get;
+            set;
+        }
+    }
+}
